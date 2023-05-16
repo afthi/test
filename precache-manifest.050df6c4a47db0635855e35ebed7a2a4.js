331f9ba98681a5a013b632f8b51a49305c9c6ffa
Fertig <alexander.fertig@thi.de>
2023-05-16 14:00:04 +0200

    Updates

---
+++ precache-manifest.050df6c4a47db0635855e35ebed7a2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5388ad1048ed88f30557",
+    "url": "/test/static/css/main.3d382945.chunk.css"
+  },
+  {
+    "revision": "5388ad1048ed88f30557",
+    "url": "/test/static/js/main.5388ad10.chunk.js"
+  },
+  {
+    "revision": "d41fcd46c642c4af341e",
+    "url": "/test/static/js/1.d41fcd46.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "5e50e7f05192c176a0fe768bba470a1c",
+    "url": "/test/index.html"
+  }
+];
